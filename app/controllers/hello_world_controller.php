af1124d3780a51d70eb722ea9c270bb1e1a28877
Sara Tolvanen <saratolv@dhcp-asv-121.helsinki.fi>
2016-03-31 21:13:12 +0300

    netbeans juttuja

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,38 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+require 'app/models/tehtävä.php';
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+class HelloWorldController extends BaseController {
+
+    public static function index() {
+// make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+// Testaa koodiasi täällä
+        View::make('helloworld.html');
+        $lasku = Tehtävä::find(1);
+        $tehtävät = Tehtävä::all();
+// Kint-luokan dump-metodi tulostaa muuttujan arvon
+        Kint::dump($tehtävät);
+        Kint::dump($lasku);
     }
-  }
+
+    public static function muistilista() {
+        View::make('suunnitelmat/muistilista.html');
+    }
+
+    public static function muokkaa() {
+        View::make('suunnitelmat/muokkaa.html');
+    }
+
+    public static function kirjaudu() {
+        View::make('suunnitelmat/kirjautuminen.html');
+    }
+
+    public static function kuvaus() {
+        View::make('suunnitelmat/kuvaus.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,34 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/muistilista', function() {
+    HelloWorldController::muistilista();
+});
+$routes->get('/muokkaa', function() {
+    HelloWorldController::muokkaa();
+});
+$routes->get('/kirjautuminen', function() {
+    HelloWorldController::kirjaudu();
+});
+$routes->get('/kuvaus/', function() {
+    HelloWorldController::kuvaus();
+});
+$routes->get('/kuvaus/:id', function() {
+    HelloWorldController::kuvaus();
+});
+$routes->get('/tehtävä', function() {
+    TehtäväController::index();
+});
+$routes->post('/suunnitelmat', function() {
+    TehtäväController::store();
+});
+$routes->get('/suunnitelmat/new', function() {
+    TehtäväController::create();
+});
