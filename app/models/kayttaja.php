2f75ca8bd7bd19589c473aa15e56824d0e086cb6
Sara Tolvanen <saratolv@dhcp-asv-121.helsinki.fi>
2016-04-18 16:33:42 +0300

    commit

--- app/models/kayttaja.php
+++ app/models/kayttaja.php
@@ -7,23 +7,21 @@ class Kayttaja extends BaseModel {
     public function __construct($attributes) {
         parent::__construct($attributes);
     }
-
-    public static function all() {
-        $kysely = DB::connection()->prepare('SELECT * FROM Kayttaja');
-        $kysely->execute();
-        $rivit = $kysely->fetchAll();
-        $kayttajat = array();
-
-        foreach ($rivit as $rivi) {
-            $kayttajat[] = new Kayttaja(array(
-                'id' => $rivi['id'],
-                'kayttajatunnus' => $rivi['kayttajatunnus'],
-                'salasana' => $rivi['salasana']
-            ));
+    
+    public static function tarkista($kayttajatunnus, $salasana) {
+        $kysely = DB::connection()->prepare('SELECT * FROM Kayttaja WHERE kayttajatunnus = :kayttajatunnus and salasana = :salasana LIMIT 1');
+        $kysely->execute(array('kayttajatunnus' => $kayttajatunnus, 'salasana' => $salasana));
+        $rivi = $kysely->fetch();
+        if($rivi) {
+          return new Kayttaja(array(
+            'id' => $rivi['id'],
+            'kayttajatunnus' => $rivi['kayttajatunnus'],
+            'salasana' => $rivi['salasana']
+          ));
+        } else {
+          return null;
         }
-
-        return $kayttajat;
-    }
+      }
 
     public static function find($id) {
         $kysely = DB::connection()->prepare('SELECT * FROM Kayttaja WHERE id = :id LIMIT 1');
@@ -44,13 +42,9 @@ class Kayttaja extends BaseModel {
     }
 
     public function save() {
-        
         $kysely = DB::connection()->prepare('INSERT INTO Kayttaja (kayttajanimi, salsana) VALUES (:kayttajanimi, :salasana) RETURNING id');
-        
         $kysely->execute(array('kayttajanimi' => $this->kayttajanimi, 'salsana' => $this->salsana));
-        
         $rivi = $kysely->fetch();
-        
         $this->id = $rivi['id'];
     }
 
