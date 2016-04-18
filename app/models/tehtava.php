971adae8d3e15f1c74266adede51391397b3fedc
Sara Tolvanen <saratolv@dhcp-asv-121.helsinki.fi>
2016-04-18 14:23:02 +0300

    tehtavamalli

--- app/models/tehtava.php
+++ app/models/tehtava.php
@@ -2,53 +2,109 @@
 
 class Tehtava extends BaseModel {
 
-    public $id, $kayttajaid, $nimi, $kuvaus, $prioriteetti, $lisayspaiva;
+    public $id, $kayttajaid, $nimi, $kuvaus, $prioriteetti, $lisayspaiva, $luokat;
 
     public function __construct($attributes) {
         parent::__construct($attributes);
     }
 
     public static function all() {
-        $query = DB::connection()->prepare('SELECT * FROM Tehtava');
-        $query->execute();
-        $rows = $query->fetchAll();
+        $kysely = DB::connection()->prepare('SELECT * FROM Tehtava WHERE kayttajaid = :kayttajaid');
+        $kysely->execute(array('kayttajaid' => $_SESSION['kayttajaid']));
+        $rivit = $kysely->fetchAll();
         $tehtavat = array();
-        foreach ($rows as $row) {
+
+        foreach ($rivit as $rivi) {
+            $luokat = array();
+            $luokkakysely = DB::connection()->prepare('SELECT Luokka.id, Luokka.nimi FROM Tehtava, Luokka, TehtavanLuokka WHERE TehtavanLuokka.tehtavaid = Tehtava.id and TehtavanLuokka.luokkaid = Luokka.id and Tehtava.id = :id');
+            $luokkakysely->execute(array('id' => $rivi['id']));
+            $luokkarivit = $luokkakysely->fetchAll();
+            foreach ($luokkarivit as $luokkarivi) {
+                $luokat[] = new Luokka(array(
+                    'id' => $luokkarivi['id'],
+                    'nimi' => $luokkarivi['nimi']
+                ));
+            }
             $tehtavat[] = new Tehtava(array(
-                'id' => $row['id'],
-                'kayttajaid' => $row['kayttajaid'],
-                'nimi' => $row['nimi'],
-                'kuvaus' => $row['kuvaus'],
-                'prioriteetti' => $row['prioriteetti'],
-                'lisayspaiva' => $row['lisayspaiva']
+                'id' => $rivi['id'],
+                'kayttajaid' => $rivi['kayttajaid'],
+                'nimi' => $rivi['nimi'],
+                'kuvaus' => $rivi['kuvaus'],
+                'prioriteetti' => $rivi['prioriteetti'],
+                'lisayspaiva' => $rivi['lisayspaiva'],
+                'luokat' => $luokat
             ));
+            unset($luokat);
         }
+
         return $tehtavat;
     }
 
     public static function find($id) {
-        $query = DB::connection()->prepare('SELECT * FROM Tehtava WHERE id = :id LIMIT 1');
-        $query->execute(array('id' => $id));
-        $row = $query->fetch();
-        if ($row) {
-            $tehtava = new Tehtava(array(
-                'id' => $row['id'],
-                'kayttajaid' => $row['kayttajaid'],
-                'nimi' => $row['nimi'],
-                'kuvaus' => $row['kuvaus'],
-                'prioriteetti' => $row['prioriteetti'],
-                'lisayspaiva' => $row['lisayspaiva']
+        $kysely = DB::connection()->prepare('SELECT * FROM Tehtava WHERE id = :id LIMIT 1');
+        $kysely->execute(array('id' => $id));
+        $rivi = $kysely->fetch();
+
+        $luokkakysely = DB::connection()->prepare('SELECT Luokka.id, Luokka.nimi FROM Tehtava, Luokka, TehtavanLuokka WHERE TehtavanLuokka.tehtavaid = Tehtava.id and TehtavanLuokka.luokkaid = Luokka.id and Tehtava.id = :id');
+        $luokkakysely->execute(array('id' => $id));
+        $luokkarivit = $luokkakysely->fetchAll();
+        $luokat = array();
+        foreach ($luokkarivit as $luokkarivi) {
+            $luokat[] = new Luokka(array(
+                'id' => $luokkarivi['id'],
+                'nimi' => $luokkarivi['nimi']
             ));
-            return $tehtava;
         }
-        return null;
+
+        if ($rivi) {
+            $tehtava = new Tehtava(array(
+                'id' => $rivi['id'],
+                'kayttajaid' => $rivi['kayttajaid'],
+                'nimi' => $rivi['nimi'],
+                'kuvaus' => $rivi['kuvaus'],
+                'prioriteetti' => $rivi['prioriteetti'],
+                'lisayspaiva' => $rivi['lisayspaiva'],
+                'luokat' => $luokat
+            ));
+        }
+        return $tehtava;
     }
 
     public function save() {
-        $query = DB::connection()->prepare('INSERT INTO Tehtava (kayttajaid, nimi, kuvaus, prioriteetti, lisayspaiva) VALUES (:kayttajaid, :nimi, :kuvaus, :prioriteetti, :lisayspaiva) RETURNING id');
-        $query->execute(array('kayttajaid' => $this->kayttajaid, 'nimi' => $this->nimi, 'kuvaus' => $this->kuvaus, 'prioriteetti' => $this->prioriteetti, 'lisayspaiva' => $this->lisayspaiva));
-        $row = $query->fetch();
-        $this->id = $row['id'];
+        $query = DB::connection()->prepare('INSERT INTO Tehtava (kayttajaid, nimi, kuvaus, prioriteetti, lisayspaiva) VALUES (:kayttajaid, :nimi, :kuvaus, :prioriteetti, now()) RETURNING id');
+        $query->execute(array('kayttajaid' => $this->kayttajaid, 'nimi' => $this->nimi, 'kuvaus' => $this->kuvaus, 'prioriteetti' => $this->prioriteetti));
+        $rivi = $kysely->fetch();
+        $this->id = $rivi['id'];
+
+        foreach ($this->luokat as $luokka) {
+            $luokkakysely = DB::connection()->prepare('INSERT INTO TehtavanLuokka (tehtavaid, luokkaid) VALUES (:tehtavaid, :luokkaid)');
+            $luokkakysely->execute(array('tehtavaid' => $this->id, 'luokkaid' => $luokkaid));
+            $rivi = $kysely->fetch();
+        }
+    }
+
+    public function paivita() {
+        $kysely = DB::connection()->prepare('UPDATE Tehtava set (nimi, kuvaus, prioriteetti) = (:nimi, :kuvaus, :prioriteetti) WHERE id = :id');
+        $kysely->execute(array('id' => $this->id, 'nimi' => $this->nimi, 'kuvaus' => $this->kuvaus, 'prioriteetti' => $this->prioriteetti));
+        $rivi = $kysely->fetch();
+        $tyhjenna_luokat = DB::connection()->prepare('DELETE FROM TehtavanLuokka WHERE tehtavaid = :tehtavaid');
+        $tyhjenna_luokat->execute(array('tehtava' => $this->id));
+        $rivi = $kysely->fetch();
+        $luokat = $this->luokat;
+        foreach ($luokat as $luokka) {
+            $luokkakysely = DB::connection()->prepare('INSERT INTO TehtavanLuokka (tehtavaid, luokkaid) VALUES (:tehtavaid, :luokkaid)');
+            $luokkakysely->execute(array('tehtavaid' => $this->id, 'luokkaid' => $luokkaid));
+            $rivi = $kysely->fetch();
+        }
+    }
+
+    public function poista() {
+        $luokkakysely = DB::connection()->prepare('DELETE FROM TehtavanLuokka WHERE tehtavaid = :tehtavaid');
+        $luokkakysely->execute(array('tehtava' => $this->id));
+        $rivi = $luokkakysely->fetch();
+        $kysely = DB::connection()->prepare('DELETE FROM Tehtava Where id = :id');
+        $kysely->execute(array('id' => $this->id));
+        $rivi = $kysely->fetch();
     }
 
 }
