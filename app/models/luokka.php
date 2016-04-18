af80d4d6fb823e8fefa9270bad3f26b2b87e1645
Sara Tolvanen <saratolv@dhcp-asv-121.helsinki.fi>
2016-04-18 13:51:18 +0300

    luokkamalli

--- app/models/luokka.php
+++ app/models/luokka.php
@@ -9,11 +9,10 @@ class Luokka extends BaseModel {
     }
 
     public static function all() {
-        $kysely = DB::connection()->prepare('SELECT * FROM Luokka');
-        $kysely->execute();
+        $kysely = DB::connection()->prepare('SELECT * FROM Luokka WHERE kayttajaid = :kayttajaid');
+        $kysely->execute(array('kayttajaid' => $_SESSION['kayttajaid']));
         $rivit = $kysely->fetchAll();
         $luokat = array();
-        
 
         foreach ($rivit as $rivi) {
             $luokat[] = new Luokka(array(
@@ -38,15 +37,30 @@ class Luokka extends BaseModel {
                 'nimi' => $rivi['nimi']
             ));
         }
-        
+
         return $luokka;
     }
-    
+
     public function save() {
         $kysely = DB::connection()->prepare('INSERT INTO Luokka (kayttajaid, nimi) VALUES (:kayttajaid, :nimi) RETURNING id');
         $kysely->execute(array('kayttajaid' => $this->kayttajaid, 'nimi' => $this->nimi));
         $rivi = $kysely->fetch();
         $this->id = $rivi['id'];
+    }
+
+    public function paivita() {
+        $kysely = DB::connection()->prepare('UPDATE Luokka set (nimi) = (:nimi) WHERE id = :id');
+        $kysely->execute(array('id' => $this->id, 'nimi' => $this->nimi));
+        $rivi = $kysely->fetch();
+    }
+
+    public function poista() {
+        $kysely = DB::connection()->prepare('DELETE FROM Luokat WHERE luokka = :luokka');
+        $kysely->execute(array('luokka' => $this->id));
+        $rivi = $kysely->fetch();
+        $kysely = DB::connection()->prepare('DELETE FROM Luokka WHERE id = :id');
+        $kysely->execute(array('id' => $this->id));
+        $rivi = $kysely->fetch();
     }
 
 }
