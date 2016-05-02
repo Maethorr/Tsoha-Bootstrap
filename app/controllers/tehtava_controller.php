1193da65b01eb31bf0123c6d4e355e0fb18485f3
Sara Tolvanen <saratolv@dhcp-asv-121.helsinki.fi>
2016-05-02 16:39:16 +0300

    commit

--- app/controllers/tehtava_controller.php
+++ app/controllers/tehtava_controller.php
@@ -14,21 +14,23 @@ class TehtavaController extends BaseController {
 
     public static function store() {
         $params = $_POST;
-        $luokat = $params['luokat'];
-        $parametrit = array(
-            'kayttajaid' => $_SESSION['kayttajaid'],
-            'nimi' => $params['nimi'],
-            'kuvaus' => $params['kuvaus'],
-            'prioriteetti' => (int) $params['prioriteetti'],
-            'luokat' => array()
-        );
-        $kaikki_luokat = array();
-        foreach ($luokat as $id) {
-            $kaikki_luokat[] = new Luokka(array(
-                'id' => $id
-            ));
+        if ('luokat' != null) {
+            $luokat = $params['luokat'];
+            $parametrit = array(
+                'kayttajaid' => $_SESSION['kayttajaid'],
+                'nimi' => $params['nimi'],
+                'kuvaus' => $params['kuvaus'],
+                'prioriteetti' => (int) $params['prioriteetti'],
+                'luokat' => array()
+            );
+            $kaikki_luokat = array();
+            foreach ($luokat as $id) {
+                $kaikki_luokat[] = new Luokka(array(
+                    'id' => $id
+                ));
+            }
+            $parametrit['luokat'] = $kaikki_luokat;
         }
-        $parametrit['luokat'] = $kaikki_luokat;
         $tehtava = new Tehtava($parametrit);
         $virheet = $tehtava->errors();
         if (count($virheet) == 0) {
@@ -60,22 +62,24 @@ class TehtavaController extends BaseController {
 
     public static function paivita($id) {
         $params = $_POST;
-        $luokat = $params['luokat'];
-        $parametrit = array(
-            'id' => $id,
-            'kayttajaid' => $_SESSION['kayttajaid'],
-            'nimi' => $params['nimi'],
-            'kuvaus' => $params['kuvaus'],
-            'prioriteetti' => $params['prioriteetti'],
-            'luokat' => array()
-        );
-        $tehtavan_luokat = array();
-        foreach ($luokat as $id) {
-            $tehtavan_luokat[] = new Luokka(array(
-                'id' => $id
-            ));
+        if ('luokat' != null) {
+            $luokat = $params['luokat'];
+            $parametrit = array(
+                'id' => $id,
+                'kayttajaid' => $_SESSION['kayttajaid'],
+                'nimi' => $params['nimi'],
+                'kuvaus' => $params['kuvaus'],
+                'prioriteetti' => $params['prioriteetti'],
+                'luokat' => array()
+            );
+            $tehtavan_luokat = array();
+            foreach ($luokat as $id) {
+                $tehtavan_luokat[] = new Luokka(array(
+                    'id' => $id
+                ));
+            }
+            $parametrit['luokat'] = $tehtavan_luokat;
         }
-        $parametrit['luokat'] = $tehtavan_luokat;
         $tehtava = new Tehtava($parametrit);
         $virheet = $tehtava->errors();
         if (count($virheet) == 0) {
